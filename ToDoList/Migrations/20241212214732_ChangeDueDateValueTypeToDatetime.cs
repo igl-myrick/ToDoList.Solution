be37e88ffa4647d12f41a7fc709a455137161d19
igl-myrick <igl.myrick@gmail.com>
2024-12-12 14:10:10 -0800

    change Due Date value type from string to DateTime

---
+++ ToDoList/Migrations/20241212214732_ChangeDueDateValueTypeToDatetime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class ChangeDueDateValueTypeToDatetime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "Items",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DueDate",
+                table: "Items",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
